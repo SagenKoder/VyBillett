b4bb3ebd9d2827dd3cf2ef6a711fc59ad992512f
Alexander Sagen <alexmsagen@gmail.com>
2019-10-01 17:22:47 +0200

    Initial commit

---
+++ VyBillett/Models/Db.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace VyBillett.Models
+{
+    public class Db : DbContext
+    {
+        public Db() : base("name=VyDb")
+        {
+            Database.CreateIfNotExists();
+        }
+
+        DbSet<Category> Categories { get; set; }
+    }
+}
