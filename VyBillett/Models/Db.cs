f1a2e4ca75c50590b97f2dc15b70a5bd0d7b7567
Gaute94 <31408331+Gaute94@users.noreply.github.com>
2019-10-01 18:24:56 +0200

    Many-to-many stuff

--- VyBillett/Models/Db.cs
+++ VyBillett/Models/Db.cs
@@ -11,9 +11,26 @@ namespace VyBillett.Models
     {
         public Db() : base("name=VyDb")
         {
+            Database.Delete();
             Database.CreateIfNotExists();
         }
 
         DbSet<Category> Categories { get; set; }
+        DbSet<Departure> Departures { get; set; }
+        DbSet<Line> Lines { get; set; }
+        DbSet<Station> Stations { get; set; }
+        DbSet<Ticket> Tickets { get; set; }
+
+        DbSet<LineStation> LineStations { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<LineStation>().HasKey(p => new { p.LineId, p.StationId });
+            modelBuilder.Entity<Line>().HasMany(p => p.LineStations).WithRequired().HasForeignKey(p => p.LineId);
+            modelBuilder.Entity<Station>().HasMany(p => p.LineStations).WithRequired().HasForeignKey(p => p.StationId);
+
+            //base.OnModelCreating(modelBuilder);
+        }
+
     }
 }
---
+++ VyBillett/Models/LineStation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace VyBillett.Models
+{
+    public class LineStation
+    {
+        public int LineId { get; set; }
+        public virtual Line Line { get; set; }
+        public int StationId { get; set; }
+        public virtual Station Station { get; set; }
+        public int minutes { get; set; }
+
+        
+    }
+}
