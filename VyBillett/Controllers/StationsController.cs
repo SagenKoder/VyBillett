8190e1865e55c239f1eaabde1d22af52bed07fe1
Marius Rostad <marostad98@gmail.com>
2019-10-03 09:56:06 +0200

    Added stations controller and the corrisponding javascript for form population

---
+++ VyBillett/Controllers/StationsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using VyBillett.Models;
+
+namespace VyBillett.Controllers
+{
+    public class StationsController : Controller
+    {
+        private Db db = new Db();
+
+        protected override void Dispose(bool disposing)
+        {
+            // Denne vil dispose dbContext etter view´et er rendret ferdig!
+            // Slik kan vi aksessere alleKundene via lazy loading der.
+            // Dette har ikke noe med ToList() å gjøre.
+            // Ved bruk av Using vil vi noen ganger få problemer med dette.
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        // GET: Stations
+        public string Index()
+        {
+            var stasjoner = db.Stations.ToList();
+
+            if (stasjoner.Count < 1)
+            {
+                return "";
+            }
+
+            var serializer = new JavaScriptSerializer();
+            return serializer.Serialize(stasjoner);
+        }
+
+        public string GetStations()
+        {
+            return "Fra GetStations";
+        }
+    }
+}
---
+++ VyBillett/Scripts/form_population.js
@@ -0,0 +1,47 @@
+﻿
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict';
+    window.addEventListener('load', function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
+
+const URL = document.URL;
+
+$(window).on('load', function () {
+    let datalist_from = $('#datalist-from');
+    datalist_from.empty();
+    $.getJSON(URL + 'stations', function (data) {
+        $.each(data, function (key, entry) {
+            datalist_from.append($('<option></option>').attr('value', entry.id).text(entry.name));
+        });
+    });
+});
+
+const to_input_field = document.getElementById("date-input-to");
+
+to_input_field.onfocus = function () {
+
+    const from_input_field = document.getElementById("date-input-from");
+
+    let datalist_to = $('#datalist-to');
+    datalist_to.empty();
+
+    $.getJSON(URL + 'stations/GetStations/' + encodeURI(from_input_field), function (data) {
+        $.each(data, function (key, entry, linenumber) {
+            datalist_to.append($('<option></option>').attr('value', entry.id).text(entry.name));
+        });
+    }).fail(console.log("Error"));
+};
