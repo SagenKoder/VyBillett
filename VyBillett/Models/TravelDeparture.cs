e7b2cabd88d16ec13298223f1f5dfb4d8db8bdff
Alexander Sagen <alexmsagen@gmail.com>
2019-10-04 16:39:13 +0200

    Added logic for departureslist

---
+++ VyBillett/Models/TravelDeparture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VyBillett.Models
+{
+    public class TravelDeparture
+    {
+        public Station StationFrom { get; set; }
+        public Station StationTo { get; set; }
+        public Departure Departure { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public DateTime ArrivalTime { get; set; }
+    }
+}
