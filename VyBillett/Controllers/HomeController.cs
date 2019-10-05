6e9cd30a4ffc724000f10ed56e9a588d8cad1926
Alexander Sagen <alexmsagen@gmail.com>
2019-10-05 16:42:47 +0200

    A lot of debug prints

--- VyBillett/Controllers/HomeController.cs
+++ VyBillett/Controllers/HomeController.cs
@@ -53,6 +53,11 @@ namespace VyBillett.Controllers
                 ticketDTO.Time.Hour, 
                 ticketDTO.Time.Minute, 0);
 
+            var dateTime30Minutes = new DateTime(dateTime.ToBinary()).AddMinutes(30); // 30 minutes after
+
+            System.Diagnostics.Debug.WriteLine("dateTime " + dateTime);
+            System.Diagnostics.Debug.WriteLine("dateTime30Minutes " + dateTime30Minutes);
+
             var fromStation = db.Stations
                 .Where(s => s.Name.ToLower().Equals(from))
                 .FirstOrDefault();
@@ -70,6 +75,8 @@ namespace VyBillett.Controllers
 
             foreach (var line in lines)
             {
+                System.Diagnostics.Debug.WriteLine("Checking out line " + line.Name);
+
                 LineStation lineStationFrom = db.LineStations
                     .Where(ls => ls.Line.LineId == line.LineId)
                     .Where(ls => ls.Station.StationId == fromStation.StationId)
@@ -80,12 +87,13 @@ namespace VyBillett.Controllers
                     .Where(ls => ls.Station.StationId == toStation.StationId)
                     .FirstOrDefault();
 
+                System.Diagnostics.Debug.WriteLine("- lineStationFrom - id:" + lineStationFrom.LineStationId + " name:" + lineStationFrom.Station.Name + " line:" + lineStationFrom.Line.Name + " minutes:" + lineStationFrom.Minutes);
+
+                System.Diagnostics.Debug.WriteLine("- lineStationTo - id:" + lineStationTo.LineStationId + " name:" + lineStationTo.Station.Name + " line:" + lineStationTo.Line.Name + " minutes:" + lineStationTo.Minutes);
+
                 if (!(lineStationTo.Minutes > lineStationFrom.Minutes)) continue;
 
-                var dateTime30Minutes = new DateTime(dateTime.ToBinary()).AddMinutes(30);
-
-                System.Diagnostics.Debug.WriteLine("dateTime " + dateTime);
-                System.Diagnostics.Debug.WriteLine("dateTime30Minutes " + dateTime30Minutes);
+                System.Diagnostics.Debug.WriteLine("- fetching departures....");
 
                 List<Departure> departures = db.Departures
                     .Where(d => d.Line.LineId == line.LineId)
@@ -93,8 +101,12 @@ namespace VyBillett.Controllers
                     .Where(d => d.DateTime.CompareTo(dateTime30Minutes) < 0)
                     .ToList();
 
+                System.Diagnostics.Debug.WriteLine("- departures - count:" + departures.Count);
+
                 foreach (var departure in departures)
                 {
+                    System.Diagnostics.Debug.WriteLine("- checking out departure from line " + departure.Line.Name + " date:" + departure.DateTime);
+
                     DateTime dateFrom = new DateTime(departure.DateTime.ToBinary()).AddMinutes(lineStationFrom.Minutes);
                     DateTime dateTo = new DateTime(departure.DateTime.ToBinary()).AddMinutes(lineStationTo.Minutes);
                     TravelDeparture travelDeparture = new TravelDeparture {
