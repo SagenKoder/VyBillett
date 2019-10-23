a7f2f057b07056a550e2d57c934bfaea4c604fee
SagenKoder <alexmsagen@gmail.com>
2019-10-23 21:01:01 +0200

    Added authentication

---
+++ Model/DbUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class DbUser
+    {
+        [Key]
+        public string Navn { get; set; }
+        public byte[] Passord { get; set; }
+
+        public byte[] Salt { get; set; }
+    }
+}
---
+++ VyBillett/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VyBillett.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "Navn må oppgis")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Passord må oppgis")]
+        public string Password { get; set; }
+    }
+}
