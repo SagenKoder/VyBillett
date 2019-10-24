19a7a83985175d69a3150f862a3f718813c49b0a
SagenKoder <alexmsagen@gmail.com>
2019-10-24 13:56:42 +0200

    Added CategoryBLL

--- BLL/CategoryBLL.cs
+++ BLL/CategoryBLL.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DAL;
+using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,46 @@ namespace BLL
 {
     public class CategoryBLL
     {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryBLL()
+        {
+            _categoryRepository = new CategoryRepository();
+        }
+
+        public CategoryBLL(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public List<Category> Get()
+        {
+            return _categoryRepository.Get();
+        }
+
+        public Category Get(int id)
+        {
+            return _categoryRepository.Get(id);
+        }
+
+        public Category Get(string name)
+        {
+            return _categoryRepository.Get(name);
+        }
+
+        public bool Edit(int id, Category category)
+        {
+            return _categoryRepository.Edit(id, category);
+        }
+
+        public bool Insert(Category category)
+        {
+            return _categoryRepository.Insert(category);
+        }
+
+        public bool Delete(int id)
+        {
+            return _categoryRepository.Delete(id);
+        }
     }
 }
