5de5419afab0939484340997b7a51f88249b44e0
SagenKoder <alexmsagen@gmail.com>
2019-10-24 13:11:37 +0200

    Fixed routing for admin

--- VyBillett/App_Start/RouteConfig.cs
+++ VyBillett/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace VyBillett
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Admin",
+                url: "Admin/{controller}/{action}/{id}",
+                defaults: new { controller = "Admin", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
