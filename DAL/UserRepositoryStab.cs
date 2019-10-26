e5a43df9a54f38588b041587723ff6fa34f134ff
Marius Rostad <marostad98@gmail.com>
2019-10-26 22:10:10 +0200

    Finished UserRepositoryStab

--- DAL/UserRepositoryStab.cs
+++ DAL/UserRepositoryStab.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Cryptography;
 using Model;
 
 namespace DAL
@@ -16,23 +14,52 @@ namespace DAL
                 return null;
             }
 
-            var dbUser = new DbUser {Username = username};
+            var salt = CreateSalt();
+            var dbUser = new DbUser { Username = username, Password = CreateHash("password", salt), Salt = salt};
             return dbUser;
         }
 
         public DbUser Create(DbUser dbUser)
         {
-            throw new NotImplementedException();
+            if (dbUser.Username == "")
+            {
+                return null;
+            }
+            else
+            {
+                return dbUser;
+            }
         }
 
         public List<DbUser> GetAll()
         {
-            throw new NotImplementedException();
+            var userList = new List<DbUser>();
+            var salt = CreateSalt();
+            var dbUser = new DbUser { Username = "username", Password = CreateHash("password", salt), Salt = salt };
+
+            userList.Add(dbUser);
+            userList.Add(dbUser);
+            userList.Add(dbUser);
+            return userList;
         }
 
         public int Count()
         {
-            throw new NotImplementedException();
+            return 3;
+        }
+
+        private byte[] CreateSalt()
+        {
+            var csprng = new RNGCryptoServiceProvider();
+            var salt = new byte[24];
+            csprng.GetBytes(salt);
+            return salt;
+        }
+        private static byte[] CreateHash(string password, byte[] salt)
+        {
+            const int keyLength = 24;
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 1000); // 1000 angir hvor mange ganger hash funskjonen skal utføres for økt sikkerhet
+            return pbkdf2.GetBytes(keyLength);
         }
     }
 }
