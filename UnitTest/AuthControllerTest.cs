f8d4467e648d881f89de6333bc96e7add4c8f44c
Marius Rostad <marostad98@gmail.com>
2019-10-27 22:14:11 +0100

    Added an incomplete test for AuthController

---
+++ UnitTest/AuthControllerTest.cs
@@ -0,0 +1,38 @@
+﻿using BLL;
+using DAL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using Model;
+using VyBillett.Controllers;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class AuthControllerTest
+    {
+        //[TestMethod]
+        //public void Index()
+        //{
+        //    var dbUser = new DbUser();
+        //    dbUser.Username = "username";
+        //    dbUser.Password = null;
+        //    dbUser.Salt = null;
+        //    var controller = new AuthController(new UserBLL(new UserRepositoryStab()), dbUser);
+        //    var actionResult = (ViewResult)controller.Index();
+
+        //    Assert.AreEqual(actionResult.ViewName, "");
+        //}
+
+        //[TestMethod]
+        //public void Index_User_Wrong()
+        //{
+        //    var controller = new AuthController(new UserBLL(new UserRepositoryStab()));
+
+        //    var actionResult = (RedirectToRouteResult)controller.Index(null);
+
+        //    Assert.IsNotNull(actionResult, "Not a redirect result");
+        //    Assert.IsFalse(actionResult.Permanent);
+        //    Assert.AreEqual("Index", actionResult.RouteValues["Action"]);
+        //}
+    }
+}
