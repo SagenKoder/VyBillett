13530d1dde9b15e3cfdd5ed754007b373040c5fb
SagenKoder <alexmsagen@gmail.com>
2019-10-27 21:36:21 +0100

    StationsControllerTest finished

--- UnitTest/StationsControllerTest.cs
+++ UnitTest/StationsControllerTest.cs
@@ -47,32 +47,48 @@ namespace UnitTest
             Assert.IsFalse(actionResult.Permanent);
             Assert.AreEqual("Index", actionResult.RouteValues["Action"]);
         }
-
+        [TestMethod]
         public void Edit()
         {
             var controller = new StationsController(new StationBLL(new StationRepositoryStab()));
             var result = (ViewResult)controller.Edit(5);
-            Assert.AreEqual(result.ViewName, "Edit");
+            Assert.AreEqual("", result.ViewName);
             var station = (Station)result.Model; 
-            Assert.AreEqual(station.StationId, 5);
+            Assert.AreEqual(0, station.StationId);
         }
-
+        [TestMethod]
         public void Edit_Post()
         {
+            var station = new Station { StationId = 1, Name = "Oslo S", LineStations = null };
+
             var controller = new StationsController(new StationBLL(new StationRepositoryStab()));
+            var actionResult = (RedirectToRouteResult) controller.Edit(station);
 
+            Assert.IsNotNull(actionResult, "Not a redirect result");
+            Assert.IsFalse(actionResult.Permanent);
+            Assert.AreEqual("Index", actionResult.RouteValues["Action"]);
         }
-
+        [TestMethod]
         public void Add()
         {
+            var station = new Station { StationId = 1, Name = "Oslo S", LineStations = null };
+
             var controller = new StationsController(new StationBLL(new StationRepositoryStab()));
-
+            var actionResult = (ViewResult)controller.Add();
+            Assert.AreEqual("", actionResult.ViewName);
+            var stationRet = (Station)actionResult.Model;
+            Assert.AreEqual(1, station.StationId);
         }
-
+        [TestMethod]
         public void Add_Post()
         {
-            var controller = new StationsController(new StationBLL(new StationRepositoryStab()));
+            var station = new Station { StationId = 1, Name = "Oslo S", LineStations = null };
 
+            var controller = new StationsController(new StationBLL(new StationRepositoryStab()));
+            var actionResult = (RedirectToRouteResult)controller.Add(station);
+            Assert.IsNotNull(actionResult, "Not a redirect result");
+            Assert.IsFalse(actionResult.Permanent);
+            Assert.AreEqual("Index", actionResult.RouteValues["Action"]);
         }
     }
 }
